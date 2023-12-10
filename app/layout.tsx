0e8239cef3cc5726c5a074d77f7645d55b0a149a
Ayden Jahola <jaholaayden@gmail.com>
2023-12-10 05:20:49 +0000

    fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
