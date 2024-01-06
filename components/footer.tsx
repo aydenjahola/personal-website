1a3f7580a94f82cf256debacaacc3636bd6133d7
Ayden Jahola <jaholaayden@gmail.com>
2024-01-06 17:19:40 +0000

    make the year dynamic

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2023 Ayden Jahola. All rights reserved.
+        &copy; {currentYear} Ayden Jahola. All rights reserved.
       </small>
     </footer>
   );
